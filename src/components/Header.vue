8c0adead7eab7c71a6ffac5d188b8cec8e9ef283
ndyanx <randybryan21ic@gmail.com>
2026-02-11 20:46:11 -0500

    fix header remove button tasks

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -312,36 +312,6 @@ const handleUserAction = () => {
                                 </button>
                             </div>
                         </div>
-
-                        <!-- Sección Borrar Todo -->
-                        <div class="sidebar-section">
-                            <h3>Acciones</h3>
-                            <button
-                                @click="
-                                    emit('show-tasks');
-                                    showSidebar = false;
-                                "
-                                class="delete-all-mobile-btn"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="20"
-                                    height="20"
-                                    viewBox="0 0 24 24"
-                                    fill="none"
-                                    stroke="currentColor"
-                                    stroke-width="2"
-                                >
-                                    <polyline points="3 6 5 6 21 6" />
-                                    <path
-                                        d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"
-                                    />
-                                    <line x1="10" y1="11" x2="10" y2="17" />
-                                    <line x1="14" y1="11" x2="14" y2="17" />
-                                </svg>
-                                Gestionar tareas
-                            </button>
-                        </div>
                     </div>
                 </aside>
             </div>
